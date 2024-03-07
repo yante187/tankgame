305b8d9a025d7e2c89eaae785d4402af604f0869
signal86 <signal86@github.com>
2024-03-07 14:44:10 -0600

    add: bullet mechanics and physics

--- script.js
+++ script.js
@@ -101,6 +101,11 @@ class Player {
         this.forward = 0;
         this.debounce = 999;
         this.firing = false;
+        this.enabled = true;
+        this.hit = false;
+        this.hitFrames = 0;
+        this.explosionImg = new Image();
+        this.explosionImg.src = "./images/explosion.png";
         switch (num) {
 
             case 1:
@@ -159,6 +164,12 @@ class Player {
 
     controller() {
 
+        if (this.hit) {
+            this.damage();
+            return;
+        }
+        if (!this.enabled) return;
+
         this.debounce++;
         
         if (this.turning == 1) {
@@ -187,6 +198,24 @@ class Player {
 
     }
 
+    damage() {
+
+        this.hit = true;
+        this.debounce = -999;
+        if (players.length <= 2) for (const player of players) player.enabled = false;
+        else this.enabled = false;
+
+        this.hitFrames++;
+
+        ctx.drawImage(this.explosionImg, this.x - 43, this.y - 48, this.explosionImg.width * 2, this.explosionImg.height * 2);
+
+        if (this.hitFrames > 90) {
+            players = players.filter(player => player.id != this.id);
+            if (players.length < 2) endGame();
+        }
+
+    }
+
 }
 
 
@@ -203,6 +232,8 @@ class Bullet {
         this.id = Math.random();
         this.x = x;
         this.y = y;
+        this.xF = 1;
+        this.yF = 1;
         this.type = type;
         this.speed = speed;
         this.angleRad = angleRad;
@@ -227,8 +258,8 @@ class Bullet {
 
         this.halflife ++;
         
-        this.y -= this.speed * Math.cos(this.angleRad);
-        this.x += this.speed * Math.sin(this.angleRad);
+        this.y -= this.speed * Math.cos(this.angleRad) * this.yF;
+        this.x += this.speed * Math.sin(this.angleRad) * this.xF;
         
         for (const wall of walls) {
             // console.log(wall.x);
@@ -236,14 +267,30 @@ class Bullet {
             // console.log("wall.x + wall.width: " + (wall.x + wall.width >= this.x + 3));
             // console.log("wall.y: " + (wall.y <= this.y - 3)); // UNDER
             // console.log("wall.y + wall.height: " + (wall.y + wall.height >= this.y + 3)); // OVER
-            if (wall.x <= this.x - 3 && wall.x + wall.width >= this.x + 3) {
-                if (wall.y <= this.y - 3 && wall.y + wall.height >= this.y + 3) {
+            // if (wall.x <= this.x - 3 && wall.x + wall.width >= this.x + 3) {
+                // if (wall.y <= this.y - 3 && wall.y + wall.height >= this.y + 3) {
+            if (this.x + 3 >= wall.x && this.x - 3 <= wall.x + wall.width) {
+                if (this.y + 3 >= wall.y && this.y - 3 <= wall.y + wall.height) {
                     console.log("collision");
                     this.y += this.speed * Math.cos(this.angleRad);
                     this.x -= this.speed * Math.sin(this.angleRad);
-                    this.speed *= -1;
-                    console.log(this.angleRad);
-                    this.angleRad = this.angleRad;
+                    if (wall.width == 10) this.xF *= -1;
+                    if (wall.height == 10) this.yF *= -1;
+                    // console.log(this.yF);
+                    // console.log(this.xF);
+                    // console.log(this.angleRad);
+                    // this.angleRad = this.angleRad;
+                }
+            }
+        }
+
+        // ctx.fillRect(this.x - 19, this.y - 17, 35, 36);
+        for (const tank of players) {
+            if (this.x + 3 >= tank.x - 19 && this.x - 3 <= tank.x + 16) {
+                if (this.y + 3 >= tank.y - 17 && this.y - 3 <= tank.y + 19) {
+                    console.log(`player ${tank.playerNum} has collided with a bullet`);
+                    this.remove();
+                    tank.damage();
                 }
             }
         }
@@ -278,6 +325,13 @@ class Wall {
         ctx.fillRect(this.x, this.y, this.width, this.height);
 
     }
+
+}
+
+
+function endGame() {
+
+    //
 
 }
 
